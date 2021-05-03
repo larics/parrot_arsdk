297418596d5987feccdfe81144512ca0ab19a579
Lovro Markovic <lovro.markovic@fer.hr>
2021-05-03 23:07:57 +0200

    [get_arch.py] Fix for Python3

--- script/get_arch.py
+++ script/get_arch.py
@@ -9,7 +9,7 @@ from __future__ import print_function
 import subprocess
 
 try:
-  arch = subprocess.check_output(["gcc", "-dumpmachine"]).strip().split("-")[0]
+  arch = subprocess.check_output(["gcc", "-dumpmachine"]).decode("utf-8").strip().split("-")[0]
   if arch in ["aarch64", "arm64"]:
     o = "aarch64"
   elif arch in ["x86_64", "amd64"]:
